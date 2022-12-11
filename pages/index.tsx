ebbb854c9cd82a688dde2d40ee7ebc9b032d83fa
taishikato <taishi.k0903@gmail.com>
2022-12-10 18:56:58 -0800

    fix weird height css

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,74 +11,72 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Box sx={{ height: "calc(100vh - 320px)" }}>
-        <HeaderMegaMenu />
-        <Box w="100%" sx={{ maxWidth: 1200, margin: "0 auto" }}>
-          <Box
+      <HeaderMegaMenu />
+      <Box w="100%" sx={{ maxWidth: 1200, margin: "0 auto" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          mb={80}
+          ta="center"
+        >
+          <Text
+            component="h1"
+            weight={900}
+            size={42}
+            variant="gradient"
+            gradient={{ from: "yellow", to: "indigo", deg: 45 }}
             sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            mb={80}
-            ta="center"
-          >
-            <Text
-              component="h1"
-              weight={900}
-              size={42}
-              variant="gradient"
-              gradient={{ from: "yellow", to: "indigo", deg: 45 }}
-              sx={{
-                "@media (max-width: 600px)": {
-                  fontSize: 30,
-                },
-              }}
-            >
-              Tweet Editor with AI
-            </Text>
-            <Text
-              size={42}
-              ml={5}
-              sx={{
-                "@media (max-width: 600px)": {
-                  fontSize: 30,
-                },
-              }}
-            >
-              🤖
-            </Text>
-          </Box>
-          <Box
-            component="main"
-            sx={(theme) => ({
-              display: "flex",
-              columnGap: 30,
               "@media (max-width: 600px)": {
-                flexDirection: "column",
-                padding: "0 10px",
+                fontSize: 30,
               },
-            })}
+            }}
           >
-            <Box w={600}>
-              <Textarea
-                onChange={(e) => {
-                  setUserInputText(e.target.value);
-                }}
-                placeholder="Your Tweet"
-                label="Your Tweet"
-                radius="md"
-                size="md"
-                minRows={10}
-                value={userInputText ?? ""}
-              />
-              <Box ta="right">
-                {twitter.parseTweet(userInputText ?? "").weightedLength}
-              </Box>
+            Tweet Editor with AI
+          </Text>
+          <Text
+            size={42}
+            ml={5}
+            sx={{
+              "@media (max-width: 600px)": {
+                fontSize: 30,
+              },
+            }}
+          >
+            🤖
+          </Text>
+        </Box>
+        <Box
+          component="main"
+          sx={(theme) => ({
+            display: "flex",
+            columnGap: 30,
+            "@media (max-width: 600px)": {
+              flexDirection: "column",
+              padding: "0 10px",
+            },
+          })}
+        >
+          <Box w={600}>
+            <Textarea
+              onChange={(e) => {
+                setUserInputText(e.target.value);
+              }}
+              placeholder="Your Tweet"
+              label="Your Tweet"
+              radius="md"
+              size="md"
+              minRows={10}
+              value={userInputText ?? ""}
+            />
+            <Box ta="right">
+              {twitter.parseTweet(userInputText ?? "").weightedLength}
             </Box>
-            <Box w={600}>
-              <AIMagicSidebar setUserInputText={setUserInputText} />
-            </Box>
+          </Box>
+          <Box w={600}>
+            <AIMagicSidebar setUserInputText={setUserInputText} />
           </Box>
         </Box>
       </Box>
