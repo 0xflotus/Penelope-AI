2b4b2656c834c16f74189b476768ebfbf0ddbff9
taishikato <taishi.k0903@gmail.com>
2022-12-16 11:48:36 -0800

    add alpha

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,14 +6,32 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const Logo = () => {
   return (
-    <Link href="/" passHref style={{ textDecoration: "none" }}>
+    <Link
+      href="/"
+      passHref
+      style={{
+        textDecoration: "none",
+        display: "flex",
+        alignItems: "baseline",
+        columnGap: 6,
+      }}
+    >
       <Text
         className={inter.className}
         weight={900}
         variant="gradient"
         gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+        fz="md"
       >
         Penelope
+      </Text>
+      <Text
+        fz="xs"
+        fs="italic"
+        weight={800}
+        sx={(theme) => ({ color: theme.colors.yellow[7] })}
+      >
+        ALPHA
       </Text>
     </Link>
   );
