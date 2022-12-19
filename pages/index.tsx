93f901fb4236f9c42032cca117449386d87d8672
taishikato <taishi.k0903@gmail.com>
2022-12-19 16:23:41 +0900

    add a modal to let users know nif it's generating a draft

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Text } from "@mantine/core";
+import { Box, Text, Modal } from "@mantine/core";
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import { HeaderMegaMenu } from "../components/Header";
 import { LoadingPlaceholder } from "../components/LoadingPlaceholder";
@@ -23,6 +23,7 @@ const Home: NextPage<{ authUser: any; checkingAuth: boolean }> = ({
   const isLoggedIn = useSelector((state: ReduxState) => state.isLoggedIn);
   const router = useRouter();
   const supabaseClient = useSupabaseClient();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchUserLatestItem = async () => {
@@ -33,9 +34,11 @@ const Home: NextPage<{ authUser: any; checkingAuth: boolean }> = ({
         .order("inserted_at", { ascending: false });
 
       if (data === null || data.length === 0) {
+        setIsModalOpen(true);
         const { data } = await axios.post("/api/createDraft", {
           text: defaultText,
         });
+        setIsModalOpen(false);
         router.push(`/drafts/${data.result}`);
 
         return;
@@ -55,6 +58,26 @@ const Home: NextPage<{ authUser: any; checkingAuth: boolean }> = ({
   return (
     <>
       <HeaderMegaMenu authUser={authUser} checkingAuth={checkingAuth} />
+      <Modal
+        opened={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        withCloseButton={false}
+        closeOnClickOutside={false}
+        centered
+        radius="md"
+        overlayOpacity={0.55}
+        overlayBlur={3}
+      >
+        <Text
+          ta="center"
+          size="lg"
+          weight="bold"
+          sx={(theme) => ({ color: theme.colors.gray[2] })}
+        >
+          Generating a draft...
+        </Text>
+        {/* Modal content */}
+      </Modal>
       <Box w="100%" sx={{ maxWidth: 1200, margin: "0 auto" }} mt={40}>
         <Box
           w="100%"
