052e3b21e2c4a7a4b71eff07177f9aa81f782fd1
taishikato <taishi.k0903@gmail.com>
2022-12-29 17:29:04 +0900

    update position

--- components/AutoCompleteSuggestionBox.tsx
+++ components/AutoCompleteSuggestionBox.tsx
@@ -3,7 +3,7 @@ import { ApiResponseCard } from "./ApiResponseCard";
 
 export const AutoCompleteSuggestionBox = () => {
   return (
-    <Box bottom={50} left={50} right={100} sx={{ position: "absolute" }}>
+    <Box bottom={50} left={50} right={50} sx={{ position: "absolute" }}>
       <ApiResponseCard result={"fhdjsfhjdashfjdshafjdsahlk"} />
     </Box>
   );
