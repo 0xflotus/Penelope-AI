94baa925b26cc6958ba34932dcdf9260ac0e5e54
taishikato <taishi.k0903@gmail.com>
2022-12-31 15:07:54 +0900

    done extension oage

--- pages/extension-token/index.tsx
+++ pages/extension-token/index.tsx
@@ -1,17 +1,65 @@
-import { Box, Button, Title } from "@mantine/core";
+import { Box, Button, Text, Title } from "@mantine/core";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { HeaderMegaMenu } from "../../components/Header";
+import { LoadingPlaceholder } from "../../components/LoadingPlaceholder";
 
 const ExtensionToken: NextPage<{
   authUser: any;
   checkingAuth: boolean;
 }> = ({ authUser, checkingAuth }) => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
   const [generating, setGenerating] = useState(false);
-  const generateToken = () => {
+  const [hasToken, setHasToken] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
+
+  const generateToken = async () => {
     setGenerating(true);
+    const token = `penelope-${crypto.randomUUID()}`;
+
+    // Save it on DB
+    const res = await supabaseClient
+      .from("users")
+      .update({ extension_token: token })
+      .eq("id", authUser.id)
+      .select("extension_token");
+
+    if (res.data === null) return;
+
+    setHasToken(true);
+    setToken(res.data[0].extension_token);
+
+    setGenerating(false);
   };
+
+  useEffect(() => {
+    const fetchUsersToken = async () => {
+      const { data } = await supabaseClient
+        .from("users")
+        .select("extension_token")
+        .eq("id", authUser.id)
+        .single();
+
+      if (data?.extension_token === null || data?.extension_token === "")
+        return;
+
+      setToken(data?.extension_token);
+      setHasToken(true);
+    };
+
+    if (authUser && authUser.id) fetchUsersToken();
+  }, [authUser, supabaseClient]);
+
+  if (checkingAuth)
+    return (
+      <LoadingPlaceholder authUser={authUser} checkingAuth={checkingAuth} />
+    );
+
+  if (!authUser) router.push("/");
 
   return (
     <>
@@ -39,15 +87,19 @@ const ExtensionToken: NextPage<{
         >
           Your token for Penelope AI chrome extension
         </Title>
-        <Button
-          color="indigo"
-          radius="xl"
-          size="md"
-          onClick={generateToken}
-          loading={generating}
-        >
-          Generate
-        </Button>
+        {hasToken ? (
+          <Text sx={(theme) => ({ color: theme.colors.gray[2] })}>{token}</Text>
+        ) : (
+          <Button
+            color="indigo"
+            radius="xl"
+            size="md"
+            onClick={generateToken}
+            loading={generating}
+          >
+            Generate
+          </Button>
+        )}
       </Box>
     </>
   );
